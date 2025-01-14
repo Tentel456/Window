8f3801a6fb58785c12f2116eef09fa15b4370d12
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-01-14 08:43:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+root = tk.Tk()
+root.title("Изображение справа")
+root.geometry("800x600")  
+
+
+image_path = photo.jpg"  
+image = Image.open(image_path)
+photo = ImageTk.PhotoImage(image)
+
+
+label = tk.Label(root, image=photo)
+label.pack(side=tk.RIGHT, padx=10, pady=10)
+
+
+root.mainloop()
